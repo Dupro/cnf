689eecd3f90f412a177d1b6bcbfaf79aba4de0c1
Dupro <duskoprokopijevic@gmail.com>
2018-05-09 19:41:54 +0200

    login modal

--- application/views/forms/login.php
+++ application/views/forms/login.php
@@ -1,6 +1,23 @@
 <div class="container">
-    
-<form name = "loginform" action = "<?php echo site_url('Guest/login_validation') ?>" method = "post">
+
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Login
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Login form</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form name = "loginform" action = "<?php echo site_url('Guest/login_validation') ?>" method = "post">
 <?php if (isset($message))
     echo "<font color='red'>$message</font><br>";
 ?>
@@ -8,6 +25,13 @@
 <?php echo form_error("username", "<font color='red'>", "</font>"); ?><br>
     Password:<input type="password" name="password"/>
 <?php echo form_error("password", "<font color='red'>", "</font>"); ?><br>
-    <input type="submit" value="Log in"/>
-</form>
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <input type="submit" class="btn btn-secondary" value="Log in"/>
+      </div></form>
+    </div>
+  </div>
 </div>
