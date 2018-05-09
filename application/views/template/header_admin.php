e2033096fc52945071d0918d441317492f7c3ab5
shomi75 <38662849+shomi75@users.noreply.github.com>
2018-05-09 18:02:38 +0200

    header promena

--- application/views/template/header_admin.php
+++ application/views/template/header_admin.php
@@ -1,8 +1,29 @@
-<?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+ <a class="navbar-brand" href="#">
+ <img src="bird.jpg" alt="Logo" style="width:40px;">
+ </a>
+ <ul class="navbar-nav">
+ <li class="nav-item ">
+ <a class="nav-link" href="#">Home</a>
+ </li>
+ <li class="nav-item">
+ <a class="nav-link" href="#">My profile</a>
+ </li>
+ <li class="nav-item">
+ <a class="nav-link" href="#">Confereces</a>
+ </li>
+ <li class="nav-item">
+ <a class="nav-link" href="#">My conferences</a>
+ </li>
+ 
+ <li class="nav-item">
+ <a class="nav-link" href="#">Logout</a>
+ </li>
+ 
+ </ul>
+</nav>
+
 
