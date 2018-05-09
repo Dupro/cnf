e95460a86eda133ca978a9caebab308c98b9b660
shomi75 <38662849+shomi75@users.noreply.github.com>
2018-05-09 23:46:38 +0200

    promena izgleda polja za registraciju

--- application/views/forms/registration.php
+++ application/views/forms/registration.php
@@ -11,29 +11,29 @@
       </div>
       <div class="modal-body">
 
-Register:<br><br>
+
 <form name = "registration_form" action = "<?php echo site_url('Guest/registration_validation') ?>" method = "post">
 <?php if (isset($message))
     echo "<font color='red'>$message</font><br>";
 ?>
-    Username:<input type="text" name="username" value="<?php echo set_value('username') ?>"/>
-<?php echo form_error("username", "<font color='red'>", "</font>"); ?><br>
-    Password:<input type="password" name="password"/>
-<?php echo form_error("password", "<font color='red'>", "</font>"); ?><br>
-Confirm your Password:<input type="password" name="password"/>
-<?php echo form_error("password", "<font color='red'>", "</font>"); ?><br>
-First name:<input type="text" name="first_name" value="<?php echo set_value('first_name') ?>"/>
-<?php echo form_error("first_name", "<font color='red'>", "</font>"); ?><br>
-Last name:<input type="text" name="last_name" value="<?php echo set_value('last_name') ?>"/>
-<?php echo form_error("last_name", "<font color='red'>", "</font>"); ?><br>
-Phone number:<input type="text" name="phone_number" value="<?php echo set_value('phone_number') ?>"/>
-<?php echo form_error("phone_number", "<font color='red'>", "</font>"); ?><br>
-Email:<input type="text" name="email" value="<?php echo set_value('email') ?>"/>
-<?php echo form_error("email", "<font color='red'>", "</font>"); ?><br>
-Organisation:<input type="text" name="organisation" value="<?php echo set_value('organisation') ?>"/>
-<?php echo form_error("organisation", "<font color='red'>", "</font>"); ?><br>
-Date of birth:<input type="date" name="date_of_birth" value="<?php echo set_value('date_of_birth') ?>"/>
-<?php echo form_error("date_of_birth", "<font color='red'>", "</font>"); ?><br>
+    Username:<input class="form-control" type="text" name="username" value="<?php echo set_value('username') ?>"/>
+<?php echo form_error("username", "<font color='red'>", "</font>"); ?>
+    Password:<input class="form-control" type="password" name="password"/>
+<?php echo form_error("password", "<font color='red'>", "</font>"); ?>
+Confirm your Password:<input class="form-control" type="password" name="password"/>
+<?php echo form_error("password", "<font color='red'>", "</font>"); ?>
+First name:<input class="form-control" type="text" name="first_name" value="<?php echo set_value('first_name') ?>"/>
+<?php echo form_error("first_name", "<font color='red'>", "</font>"); ?>
+Last name:<input class="form-control" type="text" name="last_name" value="<?php echo set_value('last_name') ?>"/>
+<?php echo form_error("last_name", "<font color='red'>", "</font>"); ?>
+Phone number:<input class="form-control" type="text" name="phone_number" value="<?php echo set_value('phone_number') ?>"/>
+<?php echo form_error("phone_number", "<font color='red'>", "</font>"); ?>
+Email:<input class="form-control" type="text" name="email" value="<?php echo set_value('email') ?>"/>
+<?php echo form_error("email", "<font color='red'>", "</font>"); ?>
+Organisation:<input class="form-control" type="text" name="organisation" value="<?php echo set_value('organisation') ?>"/>
+<?php echo form_error("organisation", "<font color='red'>", "</font>"); ?>
+Date of birth:<input class="form-control" type="date" name="date_of_birth" value="<?php echo set_value('date_of_birth') ?>"/>
+<?php echo form_error("date_of_birth", "<font color='red'>", "</font>"); ?>
 </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
--- application/views/template/header_user.php
+++ application/views/template/header_user.php
@@ -3,35 +3,35 @@
 
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark mb-4 py-3">
     <div class="container">
- <a class="navbar-brand" href="#">
- <img src="bird.jpg" alt="Logo" style="width:40px;">
- </a>
- <ul class="navbar-nav">
- <li class="nav-item ">
- <a class="nav-link" href="#">Home</a>
- </li>
- <li class="nav-item">
- <a class="nav-link" href="#">My profile</a>
- </li>
- </ul>
+        <a class="navbar-brand" href="#">
+            <img src="bird.jpg" alt="Logo" style="width:40px;">
+        </a>
+        <ul class="navbar-nav">
+            <li class="nav-item ">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">My profile</a>
+            </li>
+        </ul>
         <ul class="nav navbar-nav ml-auto">
- <li class="nav-item">
- <a class="nav-link" href="#">Confereces</a>
- </li>
- <li class="nav-item">
- <a class="nav-link" href="#">Radovi</a>
- </li>
- <li class="nav-item">
- <a class="nav-link" href="#">Novi rad</a>
- </li>
- <li class="nav-item">
- <a class="nav-link" href="#">Review</a>
- </li>
- <li class="nav-item">
- <a class="nav-link" href="<?php echo site_url('User/logout');?>">Logout</a>
- </li>
- 
- </ul>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Confereces</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Radovi</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Novi rad</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Review</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo site_url('User/logout'); ?>">Logout</a>
+            </li>
+
+        </ul>
 </nav>
 
 
