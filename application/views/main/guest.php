04d37449bbc76ca6877daad062c0b4e24e577d8b
shomi75 <38662849+shomi75@users.noreply.github.com>
2018-05-10 15:20:46 +0200

    visina slika

--- application/views/main/guest.php
+++ application/views/main/guest.php
@@ -1,13 +1,13 @@
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="http://79.170.44.155/crabtreehall.com/wp-content/uploads/2011/06/meetings.jpg" alt="First slide">
+      <img class="d-block h-50 w-100" src="http://79.170.44.155/crabtreehall.com/wp-content/uploads/2011/06/meetings.jpg" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://static1.squarespace.com/static/58ac46ff3e00be18102ed673/t/58ac49c6725e255f93381140/1487686128697/?format=500w" alt="Second slide">
+      <img class="d-block h-50 w-100" src="https://static1.squarespace.com/static/58ac46ff3e00be18102ed673/t/58ac49c6725e255f93381140/1487686128697/?format=500w" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="http://www.fnetravel.com/english/thailandmoredestination/novotelchumphonbeachresortandgolf/novotel-chumphon-beach-resort-and-golf-Samui-meeting-room.jpg" alt="Third slide">
+      <img class="d-block h-50 w-100" src="http://www.fnetravel.com/english/thailandmoredestination/novotelchumphonbeachresortandgolf/novotel-chumphon-beach-resort-and-golf-Samui-meeting-room.jpg" alt="Third slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
