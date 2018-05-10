0d27235e0b55e670702e3cd969f0593e678dabeb
shomi75 <38662849+shomi75@users.noreply.github.com>
2018-05-10 15:58:05 +0200

    link na carusel

--- application/views/main/guest.php
+++ application/views/main/guest.php
@@ -1,21 +1,37 @@
+
+
+
+
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block h-50 w-100" src="http://79.170.44.155/crabtreehall.com/wp-content/uploads/2011/06/meetings.jpg" alt="First slide">
+    <div class="carousel-inner">
+        <div class="carousel-item active" id="carosel">
+            <img class="d-block h-50 w-100" src="http://79.170.44.155/crabtreehall.com/wp-content/uploads/2011/06/meetings.jpg" alt="First slide">
+            <div class="carousel-caption">
+                <h3><a href="">Detaljinije informacije</a></h3>
+
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block h-50 w-100" src="https://static1.squarespace.com/static/58ac46ff3e00be18102ed673/t/58ac49c6725e255f93381140/1487686128697/?format=500w" alt="Second slide">
+            <div class="carousel-caption">
+                <h3><a href="">Detaljinije informacije</a></h3>
+
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block h-50 w-100" src="http://www.fnetravel.com/english/thailandmoredestination/novotelchumphonbeachresortandgolf/novotel-chumphon-beach-resort-and-golf-Samui-meeting-room.jpg" alt="Third slide">
+            <div class="carousel-caption">
+                <h3><a href="">Detaljinije informacije</a></h3>
+
+            </div>
+        </div>
     </div>
-    <div class="carousel-item">
-      <img class="d-block h-50 w-100" src="https://static1.squarespace.com/static/58ac46ff3e00be18102ed673/t/58ac49c6725e255f93381140/1487686128697/?format=500w" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block h-50 w-100" src="http://www.fnetravel.com/english/thailandmoredestination/novotelchumphonbeachresortandgolf/novotel-chumphon-beach-resort-and-golf-Samui-meeting-room.jpg" alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
 </div>
