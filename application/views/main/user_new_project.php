05d5824c92b889ebbddbb8036648b94558b04c13
shomi75 <38662849+shomi75@users.noreply.github.com>
2018-05-11 16:00:12 +0200

    myprofile

---
+++ application/views/main/user_new_project.php
@@ -0,0 +1,53 @@
+
+<h3 style="text-align:center">My new project</h3>
+<form>
+    <div class="form-group">
+    <label for="exampleFormControlSelect2">Conferences</label>
+    <select multiple class="form-control" id="exampleFormControlSelect2">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+    <div class="form-group">
+        <label for="exampleFormControlInput1">Project name</label>
+        <input type="text" class="form-control" id="exampleFormControlInput1" >
+    </div>
+    <div class="form-group">
+        <label for="exampleFormControlInput1">Key words</label>
+        <input type="text" class="form-control" id="exampleFormControlInput1" >
+    </div>
+    <div class="form-group">
+        <label for="exampleFormControlSelect1">Section</label>
+        <select class="form-control" id="exampleFormControlSelect1">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+        </select>
+    </div>
+    <div class="form-group">
+    <label for="exampleFormControlSelect2">Autors</label>
+    <select multiple class="form-control" id="exampleFormControlSelect2">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+    <div class="form-group">
+        <label for="exampleFormControlTextarea1">Apstract</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+</form>
+<form>
+  <div class="form-group">
+    <label for="exampleFormControlFile1">Choose file</label>
+    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+  </div>
+</form>
+<button type="button" class="btn btn-success">Send</button>
