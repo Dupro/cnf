9cd3d01225a5726321ecd16eaa298ec5be22eaa8
Dupro <duskoprokopijevic@gmail.com>
2018-05-11 14:10:20 +0200

    asd

--- application/views/template/header_user.php
+++ application/views/template/header_user.php
@@ -1,12 +1,7 @@
 
-<<<<<<< HEAD
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-<link rel="stylesheet" href="css/style.css">
-=======
 <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
 <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>">
 
->>>>>>> a0aae459fee5e0e6b4697fe38e395e123069fc35
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark mb-4 py-3">
     <div class="container">
         <a class="navbar-brand" href="#">
