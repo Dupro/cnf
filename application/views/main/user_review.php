5227220b5fadd28a1aa88dfe2cdaa08f444ad2d7
shomi75 <38662849+shomi75@users.noreply.github.com>
2018-05-11 17:48:45 +0200

    review

---
+++ application/views/main/user_review.php
@@ -0,0 +1,32 @@
+
+<h3 style="text-align:center">Review</h3>
+<form>
+    <div class="form-group">
+    <label for="exampleFormControlSelect2">Conference</label>
+    <input class="form-control" type="text" name="conferencename" value="<?php echo set_value('conference') ?>"/>
+     
+    </select>
+  </div>
+   
+   
+    <div class="form-group">
+        <div class="row"> 
+            <div class="col-sm-8" ><label for="exampleFormControlSelect1">Field</label>
+        <select class="form-control" id="exampleFormControlSelect1">
+            <option>Opcije iz baze</option>
+            
+        </select>
+            </div>
+          <div class="col-sm-4" > <label for="exampleFormControlSelect1">My competences</label>
+        <select class="form-control" id="exampleFormControlSelect1">
+            <option>k=1</option>
+            <option>k=2</option>
+            <option>k=3</option>
+            <option>k=4</option>
+            <option>k=5</option>
+        </select></div>
+    </div>
+    </div>
+    
+</form>
+<button type="button" class="btn btn-success">Send</button>
