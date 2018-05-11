c3efe8636bffe51df8ddd18f7367054407a10949
Nenad <39129200+Icefyre90@users.noreply.github.com>
2018-05-12 01:12:25 +0200

    moras mi pokazati taj program cisto komsiju da xxx

--- application/controllers/Ajaxsearch.php
+++ application/controllers/Ajaxsearch.php
@@ -1,26 +1,27 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Ajaxsearch extends CI_Controller {
 
-	function index()
-	{
-		   $this->load->view('template/header_guest');
-                $this->load->view('ajaxsearch');
-     $this->load->view('template/footer');
-	}
+    function index() {
+        $data['controler'] = 'Guest';
+        $this->load->view('template/header_guest');
+        $this->load->view("forms/login.php", $data);
+        $this->load->view("forms/registration.php", $data);
+        $this->load->view('ajaxsearch');
+        $this->load->view('template/footer');
+    }
 
-	function fetch()
-	{
-		$output = '';
-		$query = '';
-		$this->load->model('ajaxsearch_model');
-		if($this->input->post('query'))
-		{
-			$query = $this->input->post('query');
-		}
-		$data = $this->ajaxsearch_model->fetch_data($query);
-		$output .= '
+    function fetch() {
+        $output = '';
+        $query = '';
+        $this->load->model('ajaxsearch_model');
+        if ($this->input->post('query')) {
+            $query = $this->input->post('query');
+        }
+        $data = $this->ajaxsearch_model->fetch_data($query);
+        $output .= '
 		<div class="table-responsive">
 					<table class="table table-bordered table-striped">
 						<tr>
@@ -29,27 +30,23 @@ class Ajaxsearch extends CI_Controller {
 							<th>Organisation</th>
 						</tr>
 		';
-		if($data->num_rows() > 0)
-		{
-			foreach($data->result() as $row)
-			{
-				$output .= '
+        if ($data->num_rows() > 0) {
+            foreach ($data->result() as $row) {
+                $output .= '
 						<tr>
-							<td>'.$row->first_name.'</td>
-							<td>'.$row->last_name.'</td>
-							<td>'.$row->organisation.'</td>
+							<td>' . $row->first_name . '</td>
+							<td>' . $row->last_name . '</td>
+							<td>' . $row->organisation . '</td>
 						</tr>
 				';
-			}
-		}
-		else
-		{
-			$output .= '<tr>
+            }
+        } else {
+            $output .= '<tr>
 							<td colspan="5">No Data Found</td>
 						</tr>';
-		}
-		$output .= '</table>';
-		echo $output;
-	}
-	
+        }
+        $output .= '</table>';
+        echo $output;
+    }
+
 }
