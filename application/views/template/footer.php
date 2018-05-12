0a4dab90d5db0f4cd8b50a70fcd67809897e2906
shomi75 <38662849+shomi75@users.noreply.github.com>
2018-05-13 00:43:41 +0200

    html

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -96,3 +96,5 @@
 <script type="text/javascript" src="<?php echo base_url("assets/js/popper.js"); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
 </footer>
+</body>
+</html>
