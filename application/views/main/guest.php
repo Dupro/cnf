33aed7664c99e20095233fbc1f3f91b9a594d754
Nenad <39129200+Icefyre90@users.noreply.github.com>
2018-05-12 12:03:29 +0200

    ako ne radi upisi nesto u bazu konferencija

--- application/views/main/guest.php
+++ application/views/main/guest.php
@@ -37,17 +37,23 @@
 </div><br>
 <div class="card-deck">
 
-    <?php foreach ($confdata as $el) { ?>
-        <div class="card">
-            <img class="card-img-top" src="https://i.pinimg.com/originals/4b/3f/9a/4b3f9ad60ef749b1533623220230727b.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title"><?php echo $el['title']; ?></h5>
-                <p class="card-text"><?php echo $el['place']; ?></p>
-                <p class="card-text"><?php echo $el['begin']; ?><br/><?php echo $el['end']; ?></p>
-                <button type="button" class="btn btn-info">Info</button>
+    <?php
+    if ($confdata === NULL) {
+        echo "Nema upisanih konferencija u bazi";
+    } else {
+        foreach ($confdata as $el) {
+            ?>
+            <div class="card">
+                <img class="card-img-top" src="https://i.pinimg.com/originals/4b/3f/9a/4b3f9ad60ef749b1533623220230727b.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $el['title']; ?></h5>
+                    <p class="card-text"><?php echo $el['place']; ?></p>
+                    <p class="card-text"><?php echo $el['begin']; ?><br/><?php echo $el['end']; ?></p>
+                    <button type="button" class="btn btn-info">Info</button>
+                </div>
             </div>
-        </div>
-        <?php
+            <?php
+        }
     }
     ?>
 
