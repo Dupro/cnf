30533b48dc76824dddebbacad80c281bd21d8a04
Nenad <39129200+Icefyre90@users.noreply.github.com>
2018-05-24 14:56:26 +0200

    ededf

--- application/views/ajaxsearch.php
+++ application/views/ajaxsearch.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Live Data Search in Codeigniter using Ajax JQuery</title>
+        <title></title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     </head>
