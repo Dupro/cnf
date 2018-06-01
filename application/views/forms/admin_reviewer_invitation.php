8f2aeff2d8775be402c2975293c303f55c2728c5
Dupro <33400386+Dupro@users.noreply.github.com>
2018-06-01 15:24:16 +0200

    reviewer inv

--- application/views/forms/admin_reviewer_invitation.php
+++ application/views/forms/admin_reviewer_invitation.php
@@ -1,19 +1,21 @@
 
 <h3 style="text-align:center">Reviewer invitation</h3>
-<form>
+<form name = "reviewerInvitation" action = "<?php echo site_url('Admin/reviewerInvitation') ?>" method = "post">    
     <div class="form-group">
     <label for="exampleFormControlSelect2">Users</label>
     <input class="form-control" type="text" name="conferencename" value="<?php echo set_value('conference') ?>"/>
      
-    </select>
   </div>
    
    
     <div class="form-group">
         <div class="row"> 
             <div class="col-sm-12" ><label for="exampleFormControlSelect1">Conferences</label>
-        <select class="form-control" id="exampleFormControlSelect1">
-            <option>Opcije iz baze</option>
+        <select class="form-control" id="conferenc" name="conferenc">
+            <option value="" hidden="" >Select Conference</option>
+            <?php foreach ($confdata as $el): ?>
+                <option value="<?php echo $el["idconference"]; ?>"><?php echo $el["title"]; ?></option>
+            <?php endforeach; ?>
             
         </select>
             </div>
@@ -23,7 +25,7 @@
     </div>
     
     <div class="form-group">
-  <label for="comment">Mesage:</label>
+  <label for="comment">Message:</label>
   <textarea class="form-control" rows="5" id="comment"></textarea>
 </div>
     
