fb950e3f2810fb4f8b9633b0096b0b01299e74ce
shomi75 <shomi75@gmail.com>
2018-06-05 16:25:44 +0200

    footer

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -65,7 +65,9 @@
 
 
 
-
+<script type="text/javascript" src="<?php echo base_url("assets/js/jquery.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/popper.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
 
     </body>
 </html>
